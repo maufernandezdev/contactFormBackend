b54540b961284812501b873da6fe76e099c7f832
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-11-22 19:39:04 -0300

    adding get to / (index)

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 9001;
 app.use(express.static(__dirname + './public'));
 app.use('/api/v1/contact', require('./routes/contact'));
 
+app.get('/', (req, res) => {
+    res.send('API funcionando! ');
+});
+
 app.get('*', (req, res) => {
     res.send('404 ruta no encontrada en el servidor');
 });
